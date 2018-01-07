08bf5e80306093993685b9ebf8a8f10d93b0f67a
Jeong-Uk Lee <julee@favorie.co>
2018-01-07 14:38:03 +0900

    Initial commit

---
+++ Mugic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mugic
+//
+//  Created by Jeong-Uk Lee on 2018. 1. 7..
+//  Copyright © 2018년 Jeong-Uk Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

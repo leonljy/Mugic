5b965c5a92c7566b746e5c1bd62b4f7da5ab465b
Jeong-Uk Lee <julee@favorie.co>
2020-03-03 23:35:07 +0900

    WIP

--- Mugic/Utils/Color.swift
+++ Mugic/Utils/Color.swift
@@ -10,40 +10,75 @@ import Foundation
 import UIKit
 
 extension UIColor {
-
+    static var mugicMain: UIColor {
+        get {
+            return #colorLiteral(red: 0, green: 0.831372549, blue: 0.7764705882, alpha: 1)
+        }
+    }
+    
+    static var mugicBlack: UIColor {
+        get {
+            return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
+    }
+    
+    static var mugicLightGray: UIColor {
+        get {
+            return #colorLiteral(red: 0.4705882353, green: 0.4705882353, blue: 0.4705882353, alpha: 1)
+        }
+    }
+    
+    static var mugicDarkGray: UIColor {
+        get {
+            return #colorLiteral(red: 0.1176470588, green: 0.1176470588, blue: 0.1176470588, alpha: 1)
+        }
+    }
+    
+    static var mugicWhite: UIColor {
+        get {
+            return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    static var mugicRed: UIColor {
+        get {
+            return #colorLiteral(red: 0.9215686275, green: 0.3529411765, blue: 0.3725490196, alpha: 1)
+        }
+    }
+    
     static var noteDefaultBackground: UIColor {
         get {
-            return UIColor(red: 239.0 / 255.0, green: 239.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
+            return self.mugicDarkGray
         }
     }
     
     static var noteHighlightedBackground: UIColor {
         get {
-            return UIColor.black
+            return self.mugicMain
         }
     }
     
     static var chordDefaultBackground: UIColor {
         get {
-            return UIColor(red: 255.0 / 255.0, green: 250.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
+            return self.mugicDarkGray
         }
     }
     
     static var chordHighlightedBackground: UIColor {
         get {
-            return UIColor.darkGray
+            return self.mugicMain
         }
     }
     
     static var pianoWhiteKey: UIColor {
         get {
-            return UIColor.white
+            return self.mugicWhite
         }
     }
     
     static var pianoBlackKey: UIColor {
         get {
-            return UIColor.black
+            return self.mugicBlack
         }
     }
     
